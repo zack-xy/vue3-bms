4470617f38b29bc345731dd911830c94dcb406db
zack zheng <zack_zhengxiyun@163.com>
2021-11-17 23:28:27 +0800

    no message

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -19,7 +19,7 @@
     </div>
     <div class="base-table">
       <div class="action">
-         <el-button type="primary">新增</el-button>
+         <el-button type="primary" @click="handleAdd(1)">新增</el-button>
          <el-button type="danger">批量删除</el-button>
       </div>
       <el-table :data="menuList"
@@ -36,13 +36,62 @@
         </el-table-column>
         <el-table-column label="操作" width="220">
           <template #default="scope">
-            <el-button @click="handleAdd(scope.row)" size="mini">新增</el-button>
+            <el-button @click="handleAdd(2, scope.row)" size="mini">新增</el-button>
             <el-button @click="handleEdit(scope.row)" size="mini">编辑</el-button>
             <el-button @click="handleDelete(scope.row)" type="danger" size="mini">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
+
+      <el-dialog
+        v-model="dialogVisible"
+        center
+        :close-on-click-modal="false"
+        :close-on-press-escape="false"
+        title="菜单新增"
+        width="40%">
+        <el-form :model="menuForm" label-width="80px" ref="createForm" :rules="menuAddRoles">
+          <el-form-item label="父级菜单" prop="parentId">
+            <el-cascader
+              style="width:100%"
+              v-model="menuForm.parentId"
+              placeholder="请选择"
+              :options="menuList"
+              :props="{ checkStrictly: true, value: '_id', label: 'menuName' }"
+              clearable/>
+              <span>不选，则直接创建一级菜单</span>
+          </el-form-item>
+          <el-form-item label="菜单类型" prop="menuType">
+            <el-radio-group v-model="menuForm.menuType">
+              <el-radio v-for="key in Object.keys(MENU_TYPE)" :label="key" :key="key">
+                {{MENU_TYPE[key]}}
+              </el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="菜单名称" prop="menuName">
+              <el-input v-model="menuForm.menuName" placeholder="请输入菜单名称" clearable ></el-input>
+          </el-form-item>
+          <el-form-item label="菜单图标" prop="icon">
+              <el-input v-model="menuForm.icon" placeholder="请输入菜单图标" clearable ></el-input>
+          </el-form-item>
+          <el-form-item label="路由地址" prop="path">
+              <el-input v-model="menuForm.path" placeholder="请输入路由地址" clearable ></el-input>
+          </el-form-item>
+          <el-form-item label="权限标识" prop="menuCode">
+              <el-input v-model="menuForm.menuCode" placeholder="请输入权限标识" clearable ></el-input>
+          </el-form-item>
+          <el-form-item label="组件路径" prop="component">
+              <el-input v-model="menuForm.component" placeholder="请输入组件路径" clearable ></el-input>
+          </el-form-item>
+          <el-form-item label="菜单状态" prop="menuState">
+            <el-select v-model="menuForm.menuState" placeholder="请选择" clearable style="width:100%">
+              <el-option v-for="key in Object.keys(MENU_STATE)" :key="key" :value="key" :label="MENU_STATE[key]">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
   </div>
 </template>
 
@@ -98,12 +147,25 @@ export default {
   data () {
     return {
       MENU_STATE: Object.freeze(MENU_STATE),
+      MENU_TYPE: Object.freeze(MENU_TYPE),
       columns: Object.freeze(columns),
       queryForm: {
-        menuName: '',
-        menuState: ''
+        menuState: '',
+        menuName: ''
       },
-      menuList: []
+      menuList: [],
+      dialogVisible: false,
+      menuForm: {
+        parentId: '',
+        menuState: '',
+        menuName: '',
+        icon: '',
+        path: '',
+        menuCode: '',
+        component: ''
+      },
+      menuAddRoles: {},
+      action: 'add'
     }
   },
   methods: {
@@ -113,8 +175,12 @@ export default {
     handleReset () {
 
     },
-    handleAdd () {
-
+    handleAdd (type, row) {
+      this.action = 'add'
+      if (type === 2) {
+        this.menuForm.parentId = [...row.parentId, row._id].filter(item => item)
+      }
+      this.dialogVisible = true
     },
     handleEdit () {
 
